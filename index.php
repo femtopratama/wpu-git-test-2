eec43e5f0f6e99bf5343068cc1cba919a176bffd
femtopratama <femtopratama@yahoo.com>
2026-04-06 13:56:20 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <h1>PHP 8.2 - Readonly Classes</h1>
 <p>In PHP 8.2, you can declare an entire class as readonly. This means that all properties of the class are automatically readonly, and you don't need to declare each property individually. A readonly class cannot have any mutable properties, and all properties must be initialized at the time of
 object creation.</p>
+<p>loremsdsadasdasdassa</p>
 </body>
 </html>
